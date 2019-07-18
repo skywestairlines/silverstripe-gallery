ae1652374fa660cf2c4c1fb3e194d42ce5e8b6c8
Hilkiah Makemo <hilkiah.makemo@skywest.com>
2019-07-18 00:58:01 -0600

    Updated galler extns and assets

--- src/extn/Fancybox2_ControllerExtension.php
+++ src/extn/Fancybox2_ControllerExtension.php
@@ -10,14 +10,14 @@ class Fancybox2_ControllerExtension extends DataExtension
 
     public function onAfterInit()
     {
-        Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
+        // Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
 
-        Requirements::combine_files('fancebox2.js', array(
-            'gallery/javascript/fancybox2/jquery.fancybox.js',
-            'gallery/javascript/fancybox2/GalleryPage.js',
+        Requirements::combine_files('skywest/ss-gallery:fancebox2.js', array(
+            'skywest/ss-gallery:javascript/fancybox2/jquery.fancybox.js',
+            'skywest/ss-gallery:javascript/fancybox2/GalleryPage.js',
         ));
         Requirements::combine_files('fancybox2.css', array(
-            'gallery/css/fancybox2/jquery.fancybox.css',
+            'skywest/ss-gallery:css/fancybox2/jquery.fancybox.css',
         ));
     }
 }
